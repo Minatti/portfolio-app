5766fd4d88e6b2edb5934c15fab9d69bb47100d5
Luan Carminatti <luancarminatt@gmail.com>
2025-06-16 17:21:36 -0300

    importado routerview e adicionado os componentes dentro do template

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
-import Header from './components/Header.vue';
+import Header from '@/components/Header.vue';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <Header></Header>
+  <div class="min-h-screen flex flex-col">
+    <Header></Header>
+    <main class="flex-grow p-4">
+      <RouterView />
+    </main>
+  </div>
 </template>
-
